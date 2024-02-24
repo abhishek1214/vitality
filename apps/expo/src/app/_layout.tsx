967f400b1aec2af29d6cd9fac0e574908f79bdd3
Abhishek <abhishek.rajamohan@gmail.com>
2024-02-24 22:43:57 +0530

    Removed unused deps

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,18 +1,13 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
-import { TRPCProvider } from "~/utils/api";
-
 import "../styles.css";
-
-import { useColorScheme } from "nativewind";
 
 // This is the main layout of the app
 // It wraps your pages with the providers they need
 export default function RootLayout() {
-  const { colorScheme } = useColorScheme();
   return (
-    <TRPCProvider>
+    <>
       {/*
           The Stack component displays the current page.
           It also allows you to configure your screens 
@@ -23,11 +18,11 @@ export default function RootLayout() {
             backgroundColor: "#f472b6",
           },
           contentStyle: {
-            backgroundColor: colorScheme == "dark" ? "#09090B" : "#FFFFFF",
+            backgroundColor: "#FFFFFF",
           },
         }}
       />
       <StatusBar />
-    </TRPCProvider>
+    </>
   );
 }
